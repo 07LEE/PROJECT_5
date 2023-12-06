e12bcfe33cd372c79888c0f0f53a91a95f88d7f4
H Lee <95900411+07LEE@users.noreply.github.com>
2023-12-06 15:17:46 +0900

    Modified some code structures and added doc

--- training/load_name_list.py
+++ training/load_name_list.py
@@ -1,12 +1,7 @@
-from .arguments import get_train_args
-
-# load name list from name_list.txt
-args = get_train_args()
-name_list_path = args.name_list_path
-
-
-# load the name list from file
-def get_alias2id(name_list_path):
+def get_alias2id(name_list_path) -> dict:
+    """
+    
+    """
     with open(name_list_path, 'r', encoding='utf-8') as fin:
         name_lines = fin.readlines()
     alias2id = {}
@@ -16,10 +11,13 @@ def get_alias2id(name_list_path):
     return alias2id
 
 
-def get_id2alias(name_list_path):
+def get_id2alias(name_list_path) -> list:
+    """
+    
+    """
+    id2alias = []
     with open(name_list_path, 'r', encoding='utf-8') as fin:
         name_lines = fin.readlines()
-    id2alias = []
-    for i, line in enumerate(name_lines):
+    for line in name_lines:
         id2alias.append(line.strip().split()[1])
     return id2alias
--- training/training_control.py
+++ training/training_control.py
@@ -1,20 +1,40 @@
-# Adjustment during training
+"""
+Module providing utility functions for training. 
+Includes functions for adjusting learning rates, 
+saving training checkpoints, and saving temporary 
+model checkpoints during training.
+
+Author: 
+"""
+
 import os
 import json
 import torch
 
+from training.arguments import get_train_args
+
+args = get_train_args()
+LOG_FATH = args.training_logs
 
 def adjust_learning_rate(optimizer, lr_decay):
     """
-    Adjust learning rate with a factor.
+    Adjust the learning rate of the optimizer.
+
+    Args:
+        optimizer (torch.optim.Optimizer): The optimizer whose learning rate will be adjusted.
+        lr_decay (float): The factor by which the learning rate will be multiplied.
     """
     for param_group in optimizer.param_groups:
         param_group['lr'] *= lr_decay
 
-
-def save_checkpoint(state, info_json, dirname):
+def save_checkpoint(state, info_json, dirname) -> None:
     """
-    Save a training checkpoint. 
+    Save a training checkpoint.
+
+    Args:
+        state (dict): Model state dictionary to be saved.
+        info_json (dict): Additional information to be saved in a JSON file.
+        dirname (str): Directory path where the checkpoint will be saved.
     """
     try:
         os.makedirs(dirname)
@@ -22,4 +42,16 @@ def save_checkpoint(state, info_json, dirname):
         pass
     with open(os.path.join(dirname, 'info.json'), 'w', encoding='utf-8') as f:
         json.dump(info_json, f, indent=4)
-    torch.save(state, os.path.join(dirname, 'csn.ckpt'))
+    torch.save(state, os.path.join(dirname, 'model.ckpt'))
+
+def save_spare(name: str, epoch, model) -> None:
+    """
+    Save a temporary checkpoint during training.
+
+    Args:
+        name (str): Name or identifier for the saved checkpoint.
+        epoch (int): Current training epoch.
+        model (torch.nn.Module): The PyTorch model whose state will be saved.
+    """
+    save = os.path.join(LOG_FATH, f'{epoch}_{name}.pth')
+    torch.save(model.state_dict(), save)
